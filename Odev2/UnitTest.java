e8d073256639f1f26979c9284104f9992d8d0694
mberktas <44065412+mberktas@users.noreply.github.com>
2019-04-26 14:31:46 +0300

    Update UnitTest.java

--- Odev2/UnitTest.java
+++ Odev2/UnitTest.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Unittest Point ve Rectangle sınıflarının test edildigi sınıftır.
  */
 package odev.pkg2;
 
