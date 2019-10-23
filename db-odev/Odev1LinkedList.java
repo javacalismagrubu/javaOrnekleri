d605e91c71c79c5c1b064d10839cb86c074bc42f
Muhammet Berktaş <44065412+mberktas@users.noreply.github.com>
2019-10-23 22:28:09 +0300

    Update Odev1LinkedList.java

--- db-odev/Odev1LinkedList.java
+++ db-odev/Odev1LinkedList.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package odev1;
 
-/**
- *
- * @author muhammet
- */
 public class Odev1LinkedList<T extends Comparable<T>> extends AbstractLinkedList<T> {
     @Override
     public void insertInOrder(T value) {
