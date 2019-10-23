171218d7be935f9b401384c1de3bc67b70f57442
Muhammet Berktaş <44065412+mberktas@users.noreply.github.com>
2019-10-23 22:28:42 +0300

    Update Odev1.java

--- db-odev/Odev1.java
+++ db-odev/Odev1.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package odev1;
 
-/**
- *
- * @author muhammet
- */
 public class Odev1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Odev1LinkedList deneme = new Odev1LinkedList();
         deneme.addFirst(10);
